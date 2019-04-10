c3e1a865090d91a14f48cf73c7391a6880662670
Albert Suarez <alsumo95@gmail.com>
2019-04-10 22:47:02 +0200

    Added first Java Example

--- java/README.md
+++ java/README.md
@@ -4,10 +4,23 @@
 
 To install all of the dependencies, please follow the next steps:
 
-1. Install Java
+1. Install Java [[Ubuntu](https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-on-ubuntu-18-04) | [Mac](https://java.com/en/download/help/mac_install.xml)]
 
 ## APIs
 
-### TODO
+### RandomDog
 
-TODO
+Random pictures of dogs.
+
+```bash
+java src/RandomDog.java
+```
+
+This script will print the JSON response of the API request. An example of JSON would be:
+
+```json
+{
+    "url": "https://random.dog/78ff9a64-55a1-4556-b37a-e6a70e4e0fa5.gif"
+}
+```
+
---
+++ java/src/RandomDog.java
@@ -0,0 +1,37 @@
+package src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class RandomDog {
+    
+    private static HttpURLConnection con;
+
+    public static void main(String[] args) throws MalformedURLException,
+            ProtocolException, IOException {
+        String url = "https://random.dog/woof.json";
+        try {
+            URL myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+            con.setRequestMethod("GET");
+            StringBuilder content;
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+                String line;
+                content = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+            System.out.println(content.toString());
+        } finally {
+            con.disconnect();
+        }
+    }
+}
